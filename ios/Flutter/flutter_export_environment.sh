a2bd50f7f9824cbb03460b9c44a5a9d99744b886
Benni Dietz <b_diet05@uni-muenster.de>
2021-08-22 22:38:35 +0200

    Changes

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/benjamindietz/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/benjamindietz/Projects/LearningProjects/Flutter/mi_card_flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/benjamindietz/Projects/InnovativeDesigns/show_off_projects/MyAdventure"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
